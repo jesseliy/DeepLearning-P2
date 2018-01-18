b8391ca9d69613ac30236a9834ea9731cbd6bfba
jesseliy <jesseliy@users.noreply.github.com>
2018-01-18 23:35:10 +0900

    Update media.py

--- media.py
+++ media.py
@@ -3,28 +3,22 @@ import webbrowser
 
 class Movie():
     """Class describe the main info of movie
-
     The __init__ method may be documented in either the class level
     docstring, or as a docstring on the __init__ method itself.
-
     Either form is acceptable, but the two should not be mixed. Choose one
     convention to document the __init__ method and be consistent with it.
-
     Note:
         Do not include the `self` parameter in the ``Args`` section.
-
     Args:
         movie_title (str): movie's title.
         movie_storyline (str): movie's storyline.
         poster_image (str): poster image's url.
         trailer_youtube (str): trailer's url
-
     Attributes:
         movie_title (str): movie's title.
         movie_storyline (str): movie's storyline.
         poster_image (str): poster image's url.
         trailer_youtube (str): trailer's url
-
     Methods:
         show_trailer(): Open trailer in webbrower
     """
@@ -34,7 +28,7 @@ class Movie():
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
-        elf.trailer_url = trailer_youtube
+        self.trailer_url = trailer_youtube
 
     # Open trailer in webbrower
     def show_trailer(self):
