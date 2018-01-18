497b7c93482cd137e89e136c2acb6a89d18abe29
jesseliy <jesseliy@users.noreply.github.com>
2018-01-18 21:19:23 +0900

    Update media.py

--- media.py
+++ media.py
@@ -2,6 +2,7 @@ import webbrowser
 
 
 class Movie():
+    # Initialization the Movie-Class
     def __init__(self, movie_title, movie_storyline, _
                  poster_image, trailer_youtube):
         self.title = movie_title
@@ -9,6 +10,6 @@ class Movie():
         self.poster_image_url = poster_image
         elf.trailer_url = trailer_youtube
 
+    # Open trailer in webbrower
     def show_trailer(self):
         webbrowser.open(self.trailer_yotube_url)
-	
